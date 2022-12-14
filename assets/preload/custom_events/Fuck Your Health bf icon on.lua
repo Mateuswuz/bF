60eedc05a12fe4e56c91ed8a4851ddef92906e59
Mateuzinholol <116360372+Mateuswuz@users.noreply.github.com>
2022-12-14 14:37:48 -0300

    Add files via upload

---
+++ assets/preload/custom_events/Fuck Your Health bf icon on.lua
@@ -0,0 +1,13 @@
+-- Event notes hooks
+function onEvent(name, value1, value2)
+    local var hideme = tonumber(value1)
+    if name == "Fuck Your Health bf icon on" then
+        if hideme == 1 then
+            doTweenAlpha('fuckYourHealthIconP1Tween','iconP1', 1, 0.3, 'linear')
+            doTweenAlpha('fuckYourHealthIconP2Tween','iconP2', 0, 0.3, 'linear')
+        end
+    end
+end
+
+-- For some ungodly reason this doesn't work unless opponent hits a note?
+-- why please ugh
